8cb44ee014d430fc61c1106d13b89ef2253375f2
Victor <victorvirtual@gmail.com>
2019-08-12 08:36:20 -0400

    corregido modelo de datos

--- GraphQLServer/schema.js
+++ GraphQLServer/schema.js
@@ -1,8 +1,9 @@
-/ schema.js
+// schema.js
 
 const schema = `
 # declare custom scalars for date as GQDate
 scalar GQDate
+
 # registration type
 type Registration {
     id: ID!
@@ -13,17 +14,19 @@ type Registration {
     password: String
     pais: String
 }
+
 type Query {
     # Return a registration by id
     Registration(id: ID!): Registration
     # Return all registrations
     Registrations(limit: Int): [Registration]
 }
+
 type Mutation {
     # Create a registration
-    createRegistration (nombres: String,apellidos: String, fn: GQDate, email: String, password: String, pais: String): Registration
+    createRegistration (nombres: String, apellidos: String, fc: GQDate, email: String, password: String, pais: String): Registration
     # Update a registration
-    updateRegistration (id: ID!, nombres: String,apellidos: String, fn: GQDate, email: String, password: String, pais: String): Registration
+    updateRegistration (id: ID!, nombres: String, apellidos: String, fc: GQDate, email: String, password: String,  pais: String): Registration
     # Delete a registration
     deleteRegistration(id: ID!): Registration
 }
